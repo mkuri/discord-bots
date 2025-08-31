3a9e3c282b49d26bf14b8b9244ab571e3c804f3a
Makoto Kurihara <mkuri8m@gmail.com>
2025-08-31 20:51:57 +0900

    feat: add test bot (#3)
    Signed-off-by: Makoto Kurihara <mkuri8m@gmail.com>

--- main.py
+++ main.py
@@ -6,3 +6,19 @@ app = FastAPI()
 def read_root():
     return {"message": "Hello from Koyeb"}
 
+
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='>', intents=intents)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+import os
+bot.run(os.environ.get("CLIENT_SECRET")
+
